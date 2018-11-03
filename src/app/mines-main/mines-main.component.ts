2cc9bd70033ceaefbf875446a8befcd4ffcc5799
uros2929 <marinko29@live.com>
2018-11-04 00:13:16 +0100

    ad

--- src/app/mines-main/mines-main.component.ts
+++ src/app/mines-main/mines-main.component.ts
@@ -6,56 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mines-main.component.scss']
 })
 export class MinesMainComponent implements OnInit {
-
+ 
   constructor() { }
-  
-  randomNumRow = Math.round(Math.random() * 8);
-  randomNumCol = Math.round(Math.random() * 8);
-  mineFieldRow = Object.keys(this.createBoard());
-  mineFieldCol = this.createBoardCol();
-
+  randomNumRow=Math.round(Math.random()*4);
+  randomNumCol=Math.round(Math.random()*4);
+  minefield={
+    rows:[]
+    
+  }
 
   ngOnInit() {
- 
+    this.createBoard();
+    this.placeMineOnSpot();
+    console.log(this.minefield)
   }
-
-  createBoard() {
-    let rows=[];
-    for (let i = 0; i < 9; i++) {
-      let row = {
-        spots: []
-      };
-      for (let y = 0; y < 9; y++) {
-        let spot = {};
-        row.spots.push(spot)
-      }
-      rows.push(row)
+  
+  createBoard(){
+    for (let i = 0; i < 5; i++) {
+     let row={
+       spots:[]
+     }
+     for (let j = 0; j < 5; j++) {
+       let spot={mine:true,content:"noBoom"};
+       row.spots.push(spot)
+       
+     }
+      this.minefield.rows.push(row)
+     
     }
-    return rows;
-
-
+    return this.minefield
   }
-  createBoardCol() {
-    for (let index = 0; index < 9; index++) {
-      return this.createBoard()[index].spots
+  getSpotOfMineField(){
+    return this.minefield.rows[this.randomNumRow].spots[this.randomNumCol].content="boom";
+  }
+  placeMineOnSpot(){
+    for (let index = 0; index < 10; index++) {
+      this.getSpotOfMineField()
     }
   }
-
-  fieldClick(event) {
-    event.preventDefault();
-    event.target.parentElement.innerHTML = `<img _ngcontent-c1="" alt="" src="empty.png">`
-    console.log(event)
-  }
-
-  // putRandomMine() {
-  //   this.rows[this.randomNumRow].spots[this.randomNumCol] = "mine";
-  //   console.log(this.rows)
-  // }
-  // putManyMines(){
-  //   for (let index = 0; index < 10; index++) {
-  //     this.putRandomMine();
-  //     console.log(this.rows)
-  //   }
-  //}
 }
-
