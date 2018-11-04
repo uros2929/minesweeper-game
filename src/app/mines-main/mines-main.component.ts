434f9666367d9ff6a617322a1532a7fec243a914
uros2929 <marinko29@live.com>
2018-11-04 18:47:49 +0100

    put random mines, put numbers, check if player win or lose

--- src/app/mines-main/mines-main.component.ts
+++ src/app/mines-main/mines-main.component.ts
@@ -6,44 +6,155 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mines-main.component.scss']
 })
 export class MinesMainComponent implements OnInit {
- 
+
   constructor() { }
- 
-  minefield={
-    rows:[]
-    
+
+  minefield = {
+    rows: []
+
   }
 
   ngOnInit() {
-    this.createBoard();
-    this.placeMineOnSpot();
+    this.startGame();
     console.log(this.minefield)
   }
-  
-  createBoard(){
-    for (let i = 0; i < 5; i++) {
-     let row={
-       spots:[]
-     }
-     for (let j = 0; j < 5; j++) {
-       let spot={mine:true,content:"noBoom"};
-       row.spots.push(spot)
-       
-     }
+  startGame() {
+    this.createBoard();
+    this.placeMinesOnField();
+    this.calAddNum();
+
+  }
+  onClick(spot){
+    spot.mine=false;
+    if(spot.content=="boom"){
+      alert('Game over !')
+      let potvrda=confirm('Play again ?');
+    if (potvrda==true) {
+      location.reload()
+    }
+    }else{
+      if (this.playerWon()) {
+        alert('YOU WON');
+        let potvrda=confirm('Play again ?');
+        if (potvrda==true) {
+          location.reload()
+        }
+      }
+    }
+   
+  }
+  createBoard() {
+    for (let i = 0; i < 9; i++) {
+      let row = {
+        spots: []
+      }
+      for (let j = 0; j < 9; j++) {
+        let spot = { mine: true, content: "noBoom" };
+        row.spots.push(spot)
+
+      }
       this.minefield.rows.push(row)
-     
+
     }
     return this.minefield
   }
-  getSpotOfMineField(){
-    let randomNumRow=Math.round(Math.random()*4),
-    randomNumCol=Math.round(Math.random()*4);
-    return this.minefield.rows[randomNumRow].spots[randomNumCol].content="boom";
+  getSpotOfMineField(row, column) {
+    return this.minefield.rows[row].spots[column];
   }
-  placeMineOnSpot(){
+  placeMineOnSpot() {
+    let randomNumRow = Math.round(Math.random() * 8),
+      randomNumColumn = Math.round(Math.random() * 8),
+      spotOfMine = this.getSpotOfMineField(randomNumRow, randomNumColumn).content = "boom";
+    return spotOfMine;
+
+  }
+
+  placeMinesOnField() {
     for (let index = 0; index < 10; index++) {
-      this.getSpotOfMineField()
+      this.placeMineOnSpot()
     }
   }
-  
+
+  addingNumbers(row, column) {
+    let thisSpot = this.minefield.rows[row].spots[column];
+    if (thisSpot.content === "boom") {
+      return;
+    }
+    let mineNub = 0
+
+    if (row > 0) {
+      if (column > 0) {
+        let spot = this.getSpotOfMineField(row - 1, column - 1)
+        if (spot.content === "boom") {
+          mineNub++;
+        }
+      }
+      let spot = this.getSpotOfMineField(row - 1, column)
+      if (spot.content === "boom") {
+        mineNub++;
+      }
+      if (column < 8) {
+        let spot = this.getSpotOfMineField(row - 1, column + 1)
+        if (spot.content === "boom") {
+          mineNub++;
+        }
+      }
+    }
+
+    if (column > 0) {
+      let spot = this.getSpotOfMineField(row, column - 1);
+      if (spot.content === "boom") {
+        mineNub++
+      }
+    }
+    if (column < 8) {
+      let spot = this.getSpotOfMineField(row, column + 1);
+      if (spot.content === "boom") {
+        mineNub++;
+      }
+    }
+    if (row < 8) {
+      if (column > 0) {
+        let spot = this.getSpotOfMineField(row + 1, column - 1)
+        if (spot.content === "boom") {
+          mineNub++;
+        }
+      }
+      let spot = this.getSpotOfMineField(row + 1, column);
+      if (spot.content === "boom") {
+        mineNub++;
+      }
+      if (column < 8) {
+        let spot = this.getSpotOfMineField(row + 1, column + 1)
+        if (spot.content === "boom") {
+          mineNub++;
+        }
+      }
+    }
+
+    if (mineNub > 0) {
+      thisSpot.content = mineNub;
+    }
+  }
+
+  calAddNum() {
+    for (let c = 0; c < 9; c++) {
+      for (let r = 0; r < 9; r++) {
+        this.addingNumbers(r, c);
+      }
+    }
+  }
+  playerWon(){
+    for (let c = 0; c < 9; c++) {
+     for (let r = 0; r < 9; r++) {
+       let spot=this.getSpotOfMineField(r,c)
+       if (spot.mine && spot.content !=="boom") {
+         return false
+       }
+     }
+      
+    }
+    return true
+  }
+
 }
